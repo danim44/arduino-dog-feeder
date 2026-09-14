1405a0c8aaa8c49d03b266a28da2601740799680
danim44 <danim44@mail.regent.edu>
2026-09-13 20:51:33 -0400

    Add files via upload

---
+++ Actual_Week_8_Project.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+Servo feeder;
+
+void setup() {
+  feeder.attach(9);
+  feeder.write(0);
+}
+
+void loop() {
+  slowMove(feeder, 90, 70, 25);
+  delay(800);
+  slowMove(feeder, 70, 100, 25);
+  delay(57600000UL);
+  slowMove(feeder, 90, 70, 25);
+  delay(800);
+  slowMove(feeder, 70, 100, 25);
+  delay(28800000UL);
+  }
+
+void slowMove(Servo &s, int startAngle, int endAngle, int stepDelay) {
+  if (startAngle < endAngle) {
+    for (int pos = startAngle; pos <= endAngle; pos++) {
+      s.write(pos);
+      delay(stepDelay);
+    }
+  } else {
+    for (int pos = startAngle; pos >= endAngle; pos--) {
+      s.write(pos);
+      delay(stepDelay);
+    }
+  }
+}
